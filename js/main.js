a953e51167aa1acf417ef1767ea9743766e7c31e
Raúl Palma Kaschel <rpalmakaschel@gmail.com>
2023-06-12 18:20:07 -0400

    Instalación de Node, i18n para traducciones, javascript para botón funcional de cambio de idiomas

--- js/main.js
+++ js/main.js
@@ -1,138 +1,128 @@
 (function ($) {
     "use strict";
-
+  
     // Spinner
     var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
+      setTimeout(function () {
+        if ($('#spinner').length > 0) {
+          $('#spinner').removeClass('show');
+        }
+      }, 1);
     };
     spinner();
-    
-    
+  
     // Initiate the wowjs
     new WOW().init();
-
-
+  
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
+      delay: 10,
+      time: 2000
     });
-
-
+  
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
+      var typed_strings = $('.typed-text').text();
+      var typed = new Typed('.typed-text-output', {
+        strings: typed_strings.split(', '),
+        typeSpeed: 100,
+        backSpeed: 20,
+        smartBackspace: false,
+        loop: true
+      });
     }
-
-
+  
     // Smooth scrolling to section
     $(".btn-scroll").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 0
-            }, 1500, 'easeInOutExpo');
-        }
-    });
-    
-
-  
-      
-      
-/// Certificados galeria
-$(document).ready(function () {
-    // Filtrar las imágenes al hacer clic en los botones de categoría
-    $('#categoria-filters li').on('click', function () {
-      var categoria = $(this).data('filter');
-      $('.container .certificado-item').hide(); // Ocultar todas las imágenes de certificados
-      $('.container ' + categoria).fadeIn(); // Mostrar las imágenes de la categoría seleccionada
-  
-      // Activar/desactivar la selección de los botones
-      $('#categoria-filters li').removeClass('active');
-      $(this).addClass('active');
+      if (this.hash !== "") {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: $(this.hash).offset().top - 0
+        }, 1500, 'easeInOutExpo');
+      }
     });
   
-    // Activar el primer botón de categoría por defecto
-    $('#categoria-filters li').first().click();
-  });
+    // Certificados galeria
+    $(document).ready(function () {
+      // Filtrar las imágenes al hacer clic en los botones de categoría
+      $('#categoria-filters li').on('click', function () {
+        var categoria = $(this).data('filter');
+        $('.container .certificado-item').hide(); // Ocultar todas las imágenes de certificados
+        $('.container ' + categoria).fadeIn(); // Mostrar las imágenes de la categoría seleccionada
   
+        // Activar/desactivar la selección de los botones
+        $('#categoria-filters li').removeClass('active');
+        $(this).addClass('active');
+      });
   
-  // Portafolio galeria
-  $(document).ready(function () {
-    $('#portfolio-filters li').on('click', function () {
-      var portfolio = $(this).data('filter');
-      $('.container .portfolio-item').hide(); // Ocultar todas las imágenes de portafolio
-      $('.container ' + portfolio).fadeIn(); // Mostrar las imágenes de la categoría seleccionada
-  
-      // Activar/desactivar la selección de los botones
-      $('#portfolio-filters li').removeClass('active');
-      $(this).addClass('active');
+      // Activar el primer botón de categoría por defecto
+      $('#categoria-filters li').first().click();
     });
   
-    // Activar el primer botón de categoría por defecto
-    $('#portfolio-filters li').first().click();
-  });
+    // Portafolio galeria
+    $(document).ready(function () {
+      $('#portfolio-filters li').on('click', function () {
+        var portfolio = $(this).data('filter');
+        $('.container .portfolio-item').hide(); // Ocultar todas las imágenes de portafolio
+        $('.container ' + portfolio).fadeIn(); // Mostrar las imágenes de la categoría seleccionada
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
+        // Activar/desactivar la selección de los botones
+        $('#portfolio-filters li').removeClass('active');
+        $(this).addClass('active');
+      });
+  
+      // Activar el primer botón de categoría por defecto
+      $('#portfolio-filters li').first().click();
+    });
+  
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
+      $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+      return false;
     });
-})(jQuery);
-
-
-const toggleContainer = document.querySelector('.toggle-container');
-
-toggleContainer.addEventListener('click', function() {
-  this.parentNode.classList.toggle('active');
-});
-
-
-
-
-
-
-
-
+  
+    const toggleContainer = document.querySelector('.toggle-container');
+    const toggleCheckbox = document.querySelector('#toggle-checkbox');
+    const languageSwitch = document.querySelector('.language-switch');
+    const toggleCircle = document.querySelector('.toggle-circle');
+  
+    toggleContainer.addEventListener('click', function () {
+      toggleCheckbox.checked = !toggleCheckbox.checked;
+      languageSwitch.classList.toggle('active');
+      toggleCircle.classList.toggle('active');
+      changeLanguage();
+    });
+  
+    toggleCircle.addEventListener('click', function () {
+      toggleCheckbox.checked = !toggleCheckbox.checked;
+      languageSwitch.classList.toggle('active');
+      toggleCircle.classList.toggle('active');
+      changeLanguage();
+    });
+  
+    function changeLanguage() {
+      var currentLang = document.documentElement.lang;
+      var newLang = currentLang === 'es' ? 'en' : 'es';
+  
+      document.documentElement.lang = newLang;
+  
+      // Actualizar los textos según el idioma seleccionado
+      if (newLang === 'es') {
+        document.getElementById('descargarCV').textContent = 'Descargar CV';
+      } else {
+        document.getElementById('descargarCV').textContent = 'Download CV';
+      }
+    }
+  
+    // Establecer el idioma inicial
+    document.documentElement.lang = 'es';
+  
+  })(jQuery);
+  
